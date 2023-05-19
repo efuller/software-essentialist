0ebb22d54701094fc28a8216e5bce774f76e9741
efuller <eric@alley.co>
2023-05-19 15:04:44 -0400

    [red]: should return a string

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,5 +4,11 @@ function fizzBuzzChecker() {}
 describe("fizzbuzz", () => {
   it('should know that fizzBuzzChecker exists', () => {
     expect(fizzBuzzChecker).toBeDefined();
-  })
+  });
+
+  it('should return a string', () => {
+    const result = fizzBuzzChecker();
+
+    expect(typeof result).toBe('string');
+  });
 });
