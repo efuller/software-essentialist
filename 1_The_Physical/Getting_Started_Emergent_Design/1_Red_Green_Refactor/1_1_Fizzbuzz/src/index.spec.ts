8296dd00a93450c0874b7ef75afe2e09e8e4bd6c
efuller <eric@alley.co>
2023-05-22 08:45:30 -0400

    [green]: should throw an Error if given a number greater than 100

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,8 @@
 
-function fizzBuzzChecker() {
+function fizzBuzzChecker(num: number) {
+  if (num > 100) {
+    throw new Error('Number cannot exceed 100.');
+  }
   return '';
 }
 
@@ -9,12 +12,12 @@ describe("fizzbuzz", () => {
   });
 
   it('should return a string', () => {
-    const result = fizzBuzzChecker();
+    const result = fizzBuzzChecker(25);
 
     expect(typeof result).toBe('string');
   });
 
-  it.only('should throw an error when 101 is passed in as an argument', () => {
-    expect(fizzBuzzChecker(101)).toThrow();
+  it('should throw an error when 101 is passed in as an argument', () => {
+    expect(() => fizzBuzzChecker(101)).toThrowError();
   })
 });
