e339d2635def5c7b8f723b955dffd20bfb2c8725
efuller <eric@alley.co>
2023-05-26 09:42:53 -0400

    [red]: failing test for passing 101 as an arg

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -33,4 +33,8 @@ describe("fizzbuzz", () => {
   it('should throw an error when 0 is passed as an argument', () => {
     expect(() => fizzBuzzChecker(0)).toThrowError();
   });
+
+  it('should throw an error when 101 is passed as an argument', () => {
+    expect(() => fizzBuzzChecker(101)).toThrowError();
+  });
 });
