8c6683b31fd2c1e1d3e222ec9faae2984584d10e
Eric Fuller <info@glassmoonmedia.com>
2024-09-05 06:52:25 -0400

    Setup exceptions so they will be recognized by our error handler

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/exceptions.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/exceptions.ts
@@ -1,29 +1,45 @@
 export class InvalidRequestBodyException extends Error {
   constructor(missingKeys: string[]) {
     super("Body is missing required key: " + missingKeys.join(", "));
+
+    this.name = "InvalidRequestBodyException";
+    Object.setPrototypeOf(this, InvalidRequestBodyException.prototype);
   }
 }
 
 export class StudentNotFoundException extends Error {
   constructor() {
     super("Student not found");
+
+    this.name = "StudentNotFoundException";
+    Object.setPrototypeOf(this, StudentNotFoundException.prototype);
   }
 }
 
 export class StudentAlreadyEnrolledException extends Error {
   constructor() {
     super("Student already enrolled");
+
+    this.name = "StudentAlreadyEnrolledException";
+    Object.setPrototypeOf(this, StudentAlreadyEnrolledException.prototype);
   }
 }
 
 export class ClassNotFoundException extends Error {
   constructor() {
     super("Class not found");
+
+    this.name = "ClassNotFoundException";
+    Object.setPrototypeOf(this, ClassNotFoundException.prototype);
   }
 }
 
-export class AssignmentNotFoundException extends Error {
+export class StudentAssignmentNotFoundException extends Error {
   constructor() {
-    super("Assignment not found");
+    super("Student assignment not found");
+
+    this.name = "StudentAssignmentNotFoundException";
+    Object.setPrototypeOf(this, StudentAssignmentNotFoundException.prototype);
   }
 }
+
