4aa8c356d691206041d225ae0354df079db4efa7
Eric Fuller <info@glassmoonmedia.com>
2024-09-10 06:16:52 -0400

    Extract student db functions into database.ts

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/database.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/database.ts
@@ -1,5 +1,96 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Student } from '@prisma/client';
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
-export { prisma }
+interface StudentPersistence {
+  createStudent(name: string): any
+  getAllStudents(): any;
+  getStudentById(id: string): any
+  getStudentSubmittedAssignments(id: string): any
+  getStudentGrades(id: string): any
+}
+
+export class Database {
+  public student: StudentPersistence;
+
+  constructor(private readonly prismaClient: PrismaClient) {
+    this.student = {
+      createStudent: this.createStudent.bind(this),
+      getAllStudents: this.getAllStudents.bind(this),
+      getStudentById: this.getStudentById.bind(this),
+      getStudentSubmittedAssignments: this.getStudentSubmittedAssignments.bind(this),
+      getStudentGrades: this.getStudentGrades.bind(this)
+    }
+  }
+
+  public async createStudent(name: string) {
+    const student = await this.prismaClient.student.create({
+      data: {
+        name
+      }
+    });
+
+    return student;
+  }
+
+  public async getAllStudents() {
+    const students = await this.prismaClient.student.findMany({
+      include: {
+        classes: true,
+        assignments: true,
+        reportCards: true
+      },
+      orderBy: {
+        name: 'asc'
+      }
+    });
+
+    return students;
+  }
+
+  public async getStudentById(id: string) {
+    const student = await this.prismaClient.student.findUnique({
+      where: {
+        id
+      },
+      include: {
+        classes: true,
+        assignments: true,
+        reportCards: true
+      }
+    });
+
+    return student;
+  }
+
+  public async getStudentSubmittedAssignments(id: string) {
+    const studentAssignments = await prisma.studentAssignment.findMany({
+      where: {
+        studentId: id,
+        status: 'submitted'
+      },
+      include: {
+        assignment: true
+      },
+    });
+
+    return studentAssignments;
+  }
+
+  public async getStudentGrades(id: string) {
+    const studentAssignments = await prisma.studentAssignment.findMany({
+      where: {
+        studentId: id,
+        status: 'submitted',
+        grade: {
+          not: null
+        }
+      },
+      include: {
+        assignment: true
+      },
+    });
+
+    return studentAssignments;
+  }
+}
--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/student.service.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/student.service.ts
@@ -1,99 +1,46 @@
-import { prisma } from "./database";
+import { Database } from "./database";
 import { StudentNotFoundException } from "./exceptions";
 
 export class StudentService {
+  constructor(private readonly db: Database) {}
+
   public async createStudent(name: string) {
-    const student = await prisma.student.create({
-      data: {
-        name
-      }
-    });
+    const student = await this.db.student.createStudent(name);
 
     return student;
   }
 
   public async getAllStudents() {
-    const students = await prisma.student.findMany({
-      include: {
-        classes: true,
-        assignments: true,
-        reportCards: true
-      },
-      orderBy: {
-        name: 'asc'
-      }
-    });
+    const students = await this.db.student.getAllStudents();
 
     return students;
   }
 
   public async getStudentById(id: string) {
-    const student = await prisma.student.findUnique({
-      where: {
-        id
-      },
-      include: {
-        classes: true,
-        assignments: true,
-        reportCards: true
-      }
-    });
-
-    if (!student) {
-      throw new StudentNotFoundException();
-    }
+    const student = await this.db.student.getStudentById(id);
 
     return student;
   }
 
   public async getStudentSubmittedAssignments(id: string) {
-    const student = await prisma.student.findUnique({
-      where: {
-        id
-      }
-    });
+    const student = await this.db.student.getStudentById(id);
 
     if (!student) {
       throw new StudentNotFoundException();
     }
 
-    const studentAssignments = await prisma.studentAssignment.findMany({
-      where: {
-        studentId: id,
-        status: 'submitted'
-      },
-      include: {
-        assignment: true
-      },
-    });
-
+    const studentAssignments = await this.db.student.getStudentSubmittedAssignments(id);
     return studentAssignments;
   }
 
   public async getStudentGrades(id: string) {
-    const student = await prisma.student.findUnique({
-      where: {
-        id
-      }
-    });
+    const student = await this.db.student.getStudentById(id);
 
     if (!student) {
       throw new StudentNotFoundException();
     }
 
-    const studentAssignments = await prisma.studentAssignment.findMany({
-      where: {
-        studentId: id,
-        status: 'submitted',
-        grade: {
-          not: null
-        }
-      },
-      include: {
-        assignment: true
-      },
-    });
-
-    return studentAssignments;
+    const studentGrades = await this.db.student.getStudentGrades(id);
+    return studentGrades;
   }
 }
